ae598cc8c3ddd02891d8da4d3d850741ceb96e2b
Anirudh Sudhir <137381184+AnirudhSudhir@users.noreply.github.com>
2023-08-14 13:08:07 +0530

    Format code and add color input

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 function createGrid(gridSize) {
-    const grid = document.querySelector('.grid');
+    const grid = document.querySelector(".grid");
     if (counter === 1) grid.innerHTML = "";
     for (let i = 1; i <= gridSize; i++) {
-        const gridColumn = document.createElement('div');
-        gridColumn.classList.add('grid-column');
+        const gridColumn = document.createElement("div");
+        gridColumn.classList.add("grid-column");
         grid.appendChild(gridColumn);
         for (let j = 1; j <= gridSize; j++) {
-            const gridRow = document.createElement('div');
-            gridRow.classList.add('grid-row');
+            const gridRow = document.createElement("div");
+            gridRow.classList.add("grid-row");
             gridColumn.appendChild(gridRow);
         }
     }
+    colourGrid();
 }
 
 function colourGrid() {
-    const gridElements = document.querySelectorAll('.grid-row');
-    gridElements.forEach(element => element.addEventListener('mouseenter', (e) => e.target.style.backgroundColor = 'blue'));
+    const gridElements = document.querySelectorAll(".grid-row");
+    const colorSelect = document.querySelector("#color-input");
+    colorSelect.addEventListener("input", (e) => (colorChoice = e.target.value));
+    const colorButton = document.querySelectorAll(".color-button");
+    colorButton.forEach((input) =>
+        input.addEventListener(
+            "input",
+            (e) => colorChoice = e.target.getAttribute("id")
+        )
+    );
+    gridElements.forEach((element) =>
+        element.addEventListener(
+            "mouseenter",
+            (e) => e.target.style.backgroundColor = `${colorChoice}`
+        )
+    );
 }
 
 function getGridSize() {
     createGrid(10);
-    const input = document.querySelector('#grid-size');
-    const button = document.querySelector('#grid-update');
-    button.addEventListener('click', (e) => {
+    const input = document.querySelector("#grid-size");
+    const button = document.querySelector("#grid-update");
+    button.addEventListener("click", (e) => {
         counter = 1;
         createGrid(Number(input.value));
     });
 }
 
 let counter = 0;
+let colorChoice = "black";
 getGridSize();
-colourGrid();
